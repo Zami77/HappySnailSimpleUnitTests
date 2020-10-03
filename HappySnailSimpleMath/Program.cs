b103b99cb5add6099e239882f5b9a17939d009f1
Dan <Chunk_77@hotmail.com>
2020-10-03 06:14:22 -0700

    Create Program.cs

---
+++ HappySnailSimpleMath/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HappySnailSimpleMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy Snail Simple Unit Tests!");
+        }
+    }
+}
